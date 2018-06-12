2c66df4f31265a3b79d71dca3207d98453c5e9b6
Jörg Stenger <joerg.stenger@exozet.com>
2018-06-12 16:45:40 +0200

    reaction now calls option url

--- src/main/java/de/jos/service/discord/discordservice/manager/ReactionAddedManager.java
+++ src/main/java/de/jos/service/discord/discordservice/manager/ReactionAddedManager.java
@@ -1,7 +1,9 @@
 package de.jos.service.discord.discordservice.manager;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 import sx.blah.discord.handle.impl.events.guild.channel.message.reaction.ReactionAddEvent;
 
 import java.util.HashMap;
@@ -12,22 +14,20 @@ import java.util.Map;
 public class ReactionAddedManager {
     @Autowired
     private EmojiHandler emojiHandler;
+    private RestTemplate restTemplate;
+
+    public ReactionAddedManager(RestTemplateBuilder restTemplateBuilder) {
+        restTemplate = restTemplateBuilder.build();
+    }
 
     public void handleAddEvent(ReactionAddEvent event) {
-        Map<String, Integer> map = emojiHandler.getEmojiNameToIndexMap();
-        String emojiName = event.getReaction().getEmoji().toString().subSequence(0, 1).toString();
+        Map<Integer, String> optionsMap = emojiHandler.getUserToOptionsMap().get(event.getAuthor());
+        if (optionsMap == null) {
+            return;
+        }
 
-        System.out.println("map: " + map.keySet() + ";  emojiName: " + emojiName + ":");
+        Integer index = emojiHandler.getEmojiNameToIndexMap().get(event.getReaction().getEmoji().toString().subSequence(0, 1).toString());
 
-        map.containsKey(emojiName);
-
-        Integer index = map.get(emojiName);
-
-
-
-        System.out.println("index: " + index);
-
-
-//        emojiHandler.getUserToOptionsMap().get(event.getAuthor()).get("");
+        restTemplate.getForObject(optionsMap.get(index), Object.class);
     }
 }
